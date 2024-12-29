3e5a7266bb581e9b5b9813f9f9cc3a93abc1a6d1
aw <limeflime@gmail.com>
2024-12-29 11:03:58 +0300

    module_3_4

---
+++ module3/module_3_4.py
@@ -0,0 +1,8 @@
+def single_root_words(root_word, *other_words):
+    return [word for word in other_words if root_word.lower() in word.lower() or word.lower() in root_word.lower()]
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
